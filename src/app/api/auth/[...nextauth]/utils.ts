0e8d41c1dcbd4f24761122fdac51a0ee0cbbf112
tod1az <t.diaz.soto@gmail.com>
2025-09-05 02:00:59 -0400

    feat:add vitest

---
+++ src/app/api/auth/[...nextauth]/utils.ts
@@ -0,0 +1,13 @@
+import bcrypt from "bcryptjs"
+
+const SALT = 10
+
+export async function hashPassword(password: string) {
+  const salt = await bcrypt.genSalt(SALT)
+  const hash = await bcrypt.hash(password, salt)
+  return hash
+}
+
+export async function compareHash(password: string, hash: string) {
+  return bcrypt.compare(password, hash)
+}
